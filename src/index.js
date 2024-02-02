b463b9c67d888b73e115870348a786e76761e902
NaveenapriyaN <naveenapriya13@gmail.com>
2024-02-02 15:16:48 +0530

    Final code

---
+++ src/index.js
@@ -0,0 +1,52 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+// Add an event listener to the refresh button
+document.getElementById("refreshButton").addEventListener("click", function() {
+  // Generate random numbers for the dice
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  // Set the image sources for the dice
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  // Update the dice images
+  document.querySelector(".img1").setAttribute("src", randomImageSource1);
+  document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
+  // Update the game result based on the dice values
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+});
